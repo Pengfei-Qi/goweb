44eb7f1f0a6213e7a1998af7a7ea0555568f85d8
pengfei.qi <847469604@qq.com>
2022-10-30 23:47:15 +0800

    调整初始化方式

--- goweb32_bells-of-ireland/dao/mysql/post_test.go
+++ goweb32_bells-of-ireland/dao/mysql/post_test.go
@@ -3,31 +3,34 @@ package mysql
 import (
 	"fmt"
 	"goweb32_bells-of-ireland/models"
+	"goweb32_bells-of-ireland/settings"
 	"testing"
-
-	"github.com/jmoiron/sqlx"
 )
 
 func init() {
-	// DSN:Data Source Name
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True",
-		"root",
-		"123456",
-		"192.168.235.233",
-		3306,
-		"bells_of_ireland")
-	// 不会校验账号密码是否正确
-	// 注意！！！这里不要使用:=，我们是给全局变量赋值，然后在main函数中使用全局变量db
-	db, _ = sqlx.Connect("mysql", dsn)
+	abc := &settings.MysqlConfig{
+		Host:         "192.168.235.233",
+		Username:     "root",
+		Password:     "123456",
+		DbName:       "bells_of_ireland",
+		Port:         3306,
+		MaxOpenConns: 100,
+		MaxIdleConns: 20,
+	}
+	err := Init(abc)
+	if err != nil {
+		fmt.Printf("mysql connect failed , because of :%s", err.Error())
+	}
+	fmt.Printf("mysql connect success")
 }
 
 func TestInsertPost(t *testing.T) {
 	postData := &models.Post{
-		ID:          12345,
-		AuthorID:    2333,
-		CommunityId: 2,
-		Title:       "test",
-		Content:     "三生三世十里桃花",
+		ID:          5632,
+		AuthorID:    2222,
+		CommunityId: 1,
+		Title:       "test3",
+		Content:     "黄林",
 	}
 
 	err := InsertPost(postData)
--- goweb32_bells-of-ireland/go.mod
+++ goweb32_bells-of-ireland/go.mod
@@ -26,6 +26,7 @@ require (
 require (
 	github.com/BurntSushi/toml v1.1.0 // indirect
 	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/adeven/go-wrk v0.0.0-20200418124433-63e11dd31fef // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
